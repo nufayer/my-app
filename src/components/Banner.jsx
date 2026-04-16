6f14e5abb51d991f69695aebc6a0b3f60c139ee8
nufayer <nufayer6543@gmail.com>
2026-04-16 12:53:33 +0600

    added banner-section

---
+++ src/components/Banner.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="hero bg-base-200 min-h-120">
+  <div className="hero-content text-center">
+    <div className="max-w-ld">
+      <h1 className="text-5xl font-bold">Friends to keep close in your life</h1>
+      <p className="py-6">
+        Your personal shelf of meaningful connections. Browse, tend, and nurture the <br/>
+         relationships that matter most.
+      </p>
+      <button className="btn btn-default bg-green-800 border-none">+ Add Friend</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Banner;
