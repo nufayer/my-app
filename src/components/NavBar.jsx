7610b57cd584002e3787e1c214881eb8d892d383
nufayer <nufayer6543@gmail.com>
2026-04-16 12:52:55 +0600

    added navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      
+    </div>
+    <a className="btn btn-ghost text-xl">KeenKeeper</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    
+  </div>
+  <div className="navbar-end gap-2">
+    <a className="btn">Home</a>
+    <a className="btn">Time Line</a>
+    <a className="btn">Stats</a>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Header;
